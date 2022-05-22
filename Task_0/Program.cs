34b572af10bc3fb8c397fc93a090efe24c6cfad4
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-22 18:31:23 +0300

    Add .ignore

---
+++ Task_0/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на самао себя)
+
+int num = 5;
+//int Result = num*num;
+double Result = Math.Pow(num, 5);
+Console.WriteLine(Result);
