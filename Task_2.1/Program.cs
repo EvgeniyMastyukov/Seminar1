f954a11f322045db2fed224e0b604ad8e83037e3
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-24 19:48:50 +0300

    Add Task 2.1

---
+++ Task_2.1/Program.cs
@@ -0,0 +1,5 @@
+﻿// Второй способ задачи Написать прогу, которая будет выдавать название дня недели
+// по заданному номеру: 3 -- среда; 5 -- пятница.
+string numweek = Console.ReadLine();
+if (numweek == "3") Console.WriteLine("Среда");
+if (numweek == "5") Console.WriteLine("пятница");
