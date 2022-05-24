31f3d65e393002a36abe0c601b4aa4622c635e70
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-24 17:28:42 +0300

    Add Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая будет выдавать название дня недели по заданному номеру
+Console.WriteLine("Введем  число 3 или 5: ");
+string num = Console.ReadLine();
+if (num == "3")
+{
+    Console.WriteLine("Сегодня среда");
+}
+else
+{
+    Console.WriteLine("Сегодня пятница");
+}
+
+//int what = new Random().Next(0,2);
+//if (what == 0 )
+//{
+//    int x = 3;
+//}
+//if (what == 1)
+//{
+//    int x = 5;
+//}
+//Console.WriteLine(x);
+//if (x == 3)
+//{
+//    Console.WriteLine("Day = Wednesday");
+//}
+//else
+//{
+  //  Console.WriteLine("Day = Friday");
+//}
