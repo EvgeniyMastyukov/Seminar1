af1ca380d3a7309efcef36457b665d5d69da4c4e
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-24 19:45:36 +0300

    Add description Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.WriteLine("Первое число a = ");
+﻿// Первый способ задачи Напишите программу, которая на вход принимает два числа и проверят, 
+//является ли первое число квадратом второго: а = 5, b =25; a = 2, b = 10. 
+
+Console.WriteLine("Первое число a = ");
 int a = int.Parse(Console.ReadLine());
 Console.WriteLine("Второе число b = ");
 int b = int.Parse(Console.ReadLine());
