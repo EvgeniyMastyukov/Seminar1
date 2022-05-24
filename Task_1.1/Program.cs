8c2ec12a7ac873c7374e6aa9dac48434ea65e7c8
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-24 19:10:01 +0300

    Add 2 var Task 1

---
+++ Task_1.1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Второй способ задачи Напишите программу, которая на вход принимает два числа и проверят, 
+//является ли первое число квадратом второго: а = 5, b =25; a = 2, b = 10. 
+int num1 = int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+bool num1sqare = num1*num1 == num2;
+Console.WriteLine(num1sqare? "Да" : "Нет");
