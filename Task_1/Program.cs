b1441162cd50e07aef7e376ab8888a801b51b72f
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-24 13:06:17 +0300

    Add Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Первое число a = ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Второе число b = ");
+int b = int.Parse(Console.ReadLine());
+
+if (a*a == b)
+{
+    Console.WriteLine("a - квадрат b");
+}
+else
+{
+    Console.WriteLine("a - не квадрат b");
+}
