df394adea661f76790ee1c95f66287d32f874116
Mastyukov Evgeniy <e.mastyukov@mail.ru>
2022-05-25 14:09:32 +0300

    add task 3

---
+++ Task_3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке 
+// от -N до  N: 4 --> -4, -3, -2, -1,0, 1,2,3,4; 2--> -2, -1, 0, 1, 2.
+int num = int.Parse(Console.ReadLine());
+int count = 0;
+Console.WriteLine(num);
+while (count <= 2*num +1)
+{
+    num = num - 1;
+    Console.WriteLine(num);
+}
+count = count+1;
+
